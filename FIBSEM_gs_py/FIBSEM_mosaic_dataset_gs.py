05e084355056029d2ae8aaf582e057a63d1b096f
Gleb Shtengel <gleb.shtengel@gmail.com>
2026-02-13 14:24:27 -0500

    cleaning up output

--- FIBSEM_gs_py/FIBSEM_mosaic_dataset_gs.py
+++ FIBSEM_gs_py/FIBSEM_mosaic_dataset_gs.py
@@ -2165,8 +2165,7 @@ class FIBSEM_mosaic_dataset:
 
             if n_matches>0:
                 fig0, axs0 = plt.subplots(1,3, figsize=(10,4))
-                fig0.suptitle(Sample_ID + ',  thr_min={:.0e}, thr_max={:.0e}, SIFT_contrastThreshold={:.3f}'.format(thr_min, thr_max, SIFT_contrastThreshold), fontsize=fsz-1)
-
+                fig0.suptitle(Sample_ID + ',  thr_min={:.0e}, thr_max={:.0e}, data range: {:.1f} ÷ {:.1f}'.format(thr_min, thr_max, dmin, dmax), fontsize=fsz-1)
                 axx = axs0[0]
                 axx.set_xlabel('SIFT: X Error (pixels)')
                 axx.set_ylabel('Count')
@@ -2176,6 +2175,7 @@ class FIBSEM_mosaic_dataset:
                 ax_int = axs0[2]
                 ax_int.set_xlabel('Img1/Img0 Key-Pt Intensity Ratio')
                 ax_int.set_ylabel('Count')
+                axs[0].text(0.01, 1.01, 'SIFT_nOctaveLayers={:d},  SIFT_edgeThreshold={:.3f}, SIFT_contrastThreshold={:.3f},  SIFT_sigma={:.3f}'.format(SIFT_nOctaveLayers, SIFT_edgeThreshold, SIFT_contrastThreshold, SIFT_sigma), fontsize=fsz-2, transform=axs[0].transAxes)
 
                 hist_int, bins_int, patches_int = ax_int.hist(int_ratios, bins = 64)
                 FWHM_int, indi_int, inda_int, mx_int, mx_int_ind = find_FWHM(bins_int, hist_int[:-1], verbose=False, estimation=estimation, start=start, max_aver_aperture=5)
@@ -2197,11 +2197,9 @@ class FIBSEM_mosaic_dataset:
                 axx.text(0.05, 0.9, 'mean={:.3f}'.format(np.mean(xshifts)), transform=axx.transAxes, fontsize=fsz)
                 axx.text(0.05, 0.8, 'median={:.3f}'.format(np.median(xshifts)), transform=axx.transAxes, fontsize=fsz)
                 axx.text(0.05, 0.7, 'FWHM={:.3f}'.format(error_FWHMx), transform=axx.transAxes, fontsize=fsz)
-                axx.set_title('data range: {:.1f} ÷ {:.1f}'.format(dmin, dmax), fontsize=fsz)
                 ycounts, ybins, yhist_patches = axy.hist(yshifts, bins=64)
                 error_FWHMy, indyi, indya, mxy, mxy_ind = find_FWHM(ybins, ycounts[:-1], verbose=False, estimation=estimation, start=start, max_aver_aperture=5)
-                dby = (ybins[1]-ybins[0
-                    ])/2.0
+                dby = (ybins[1]-ybins[0])/2.0
                 #axy.plot([ybins[indyi] + dby, ybins[indya] + dby], [mxy/2.0, mxy/2.0], 'r', linewidth = 4)
                 axy.plot([ybins[indyi], ybins[indya]], [mxy/2.0, mxy/2.0], 'r', linewidth = 4)
                 axy.plot([ybins[mxy_ind] + dby], [mxy], 'rd')
@@ -2213,6 +2211,9 @@ class FIBSEM_mosaic_dataset:
                 axt.text(0.65, 0.8, 'Transf. Matrix:', transform=axt.transAxes, fontsize=fsz)
                 axt.text(0.55, 0.7, '{:.4f} {:.4f} {:.4f}'.format(transform_matrix[0,0], transform_matrix[0,1], transform_matrix[0,2]), transform=axt.transAxes, fontsize=fsz-1)
                 axt.text(0.55, 0.6, '{:.4f} {:.4f} {:.4f}'.format(transform_matrix[1,0], transform_matrix[1,1], transform_matrix[1,2]), transform=axt.transAxes, fontsize=fsz-1)
+
+                for ax in axs0.ravel():
+                    ax.grid(True)
 
             fig, axs = plt.subplots(1, 2, figsize=(10, 5.5))
             fig.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.95, wspace=0.05)
