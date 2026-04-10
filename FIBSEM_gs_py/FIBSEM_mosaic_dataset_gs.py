8db36c1453700f351004dae7b39f14e1e40aec34
Gleb Shtengel <gleb.shtengel@gmail.com>
2026-04-10 11:22:59 -0400

    debugging remap_tile

--- FIBSEM_gs_py/FIBSEM_mosaic_dataset_gs.py
+++ FIBSEM_gs_py/FIBSEM_mosaic_dataset_gs.py
@@ -286,8 +286,11 @@ def remap_tile(img, deformation_field, **kwargs):
     verbose = kwargs.get('verbose', False)
     
     img_shape = img.shape
-    shift_x = int(np.min((np.nanmin(deformation_field[:, :, 0]), 0))) # Find the leftmost source coordinate in the entire field — but only if it is negative.
-    shift_y = int(np.min((np.nanmin(deformation_field[:, :, 1]), 0))) # Find the top-most source coordinate in the entire field — but only if it is negative.
+    #shift_x = int(np.min((np.nanmin(deformation_field[:, :, 0]), 0))) # Find the leftmost source coordinate in the entire field — but only if it is negative.
+    #shift_y = int(np.min((np.nanmin(deformation_field[:, :, 1]), 0))) # Find the top-most source coordinate in the entire field — but only if it is negative.
+    shift_x = int(np.nanmin(deformation_field[:, :, 0]))
+    shift_y = int(np.nanmin(deformation_field[:, :, 1]))
+
     if verbose:
         print('shift_x={:d},  shift_y={:d}'.format(shift_x, shift_y))
     # df_shifted = deformation_field*1.0
