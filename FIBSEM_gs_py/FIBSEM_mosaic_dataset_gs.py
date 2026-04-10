51e1a0e51257508f8f5f528c84fac2c0c2c8d82b
Gleb Shtengel <gleb.shtengel@gmail.com>
2026-04-10 10:24:38 -0400

    adding verbose outputs to transform_tile

--- FIBSEM_gs_py/FIBSEM_mosaic_dataset_gs.py
+++ FIBSEM_gs_py/FIBSEM_mosaic_dataset_gs.py
@@ -137,6 +137,8 @@ def transform_tile(tile_params, deformation_field, **kwargs):
 
     kwargs:
     -----------
+    verbose : bool
+        If True, the inntermediate results are displaued. Default is False.
 
     Returns:
     ----------
@@ -144,6 +146,7 @@ def transform_tile(tile_params, deformation_field, **kwargs):
 
     '''
     j, fl, image_name, tr_matr_single, montage_ysz, montage_xsz, weight_min, weight_max, left_crop, I0, scale = tile_params
+    verbose = kwargs.get('verbose', False)
     fr = FIBSEM_frame(fl)
     if image_name == 'RawImageB':
         tile_initial = fr.RawImageB.astype(np.float32)
@@ -160,7 +163,10 @@ def transform_tile(tile_params, deformation_field, **kwargs):
     else:
         # No distortion correction — apply registration transform only
         df = convert_tr_matr_into_deformation_field(tr_matr_single, (fr.YResolution, fr.XResolution)).astype(np.float32)
-    tile_transformed, shift_x, shift_y = remap_tile(tile_initial_rescaled, df)
+    tile_transformed, shift_x, shift_y = remap_tile(tile_initial_rescaled, df, verbose=verbose)
+    if verbose:
+        print('remap_tile returned image with shape:    ' tile_transformed.shape)
+        print('remap_tile returned shift_x={:d},  shift_y={:d}'.format(shift_x, shift_y))
     loc_szy, loc_szx = tile_transformed.shape
     # shift_x <= 0, shift_y <= 0  (remap_tile convention: negative = tile extends left/above origin)
     x0 = np.max((shift_x, 0))   # always 0
@@ -174,7 +180,14 @@ def transform_tile(tile_params, deformation_field, **kwargs):
     weight_out = build_weight_array(tile_transformed_cropped.shape, weight_min = weight_min, weight_max = weight_max)
     weight_out[np.isnan(tile_transformed_cropped)] = 0
     tile_out = np.nan_to_num(tile_transformed_cropped, copy=False, nan=0.0) * weight_out
-    return tile_out, weight_out, xi, xa-left_crop-x0, yi, ya-y0
+    xi_out = xi
+    xa_out = xa-left_crop-x0
+    yi_out = yi
+    ya_out = ya-y0
+    if verbose:
+        print('transform_tile returning image with shape:             ' tile_out.shape)
+        print('transform_tile returning tile positions  xi, xa, yi, ya': xi_out, xa_out, yi_out, ya_out)
+    return tile_out, weight_out, xi_out, xa_out, yi_out, ya_out
     
 
 def overlay_montage_grid(ax, montage_object, **kwargs):
